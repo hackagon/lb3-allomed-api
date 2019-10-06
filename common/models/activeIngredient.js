3e890233c831cca04e8b7e36b5ffc6d4b1601ce9
hackagon <phonghiavan@gmail.com>
2019-10-06 14:44:04 +0700

    clean remote hook

--- common/models/activeIngredient.js
+++ common/models/activeIngredient.js
@@ -1,25 +1,18 @@
 const _ = require('lodash');
 
 module.exports = (ActiveIngredient) => {
-  // ActiveIngredient.observe('after save', (ctx, next) => {
-  //   console.log(ctx.req)
-  //   if (ctx.instance) {
-  //     console.log('Saved %s#%s', ctx.Model.modelName, ctx.instance.id);
-  //     const { instance } = ctx;
-  //     console.log("TCL: instance", instance)
 
-  //   } else {
-  //     console.log('Updated %s matching %j',
-  //       ctx.Model.pluralModelName,
-  //       ctx.where);
-  //   }
-  //   next();
-  // })
-
-  ActiveIngredient.afterRemote('*', async (ctx) => {
-    const items = ctx.result;
-    if (Array.isArray(ctx.result) && items[0].activeIngredientCode) {
-      ctx.result = items.map(item => ({ activeIngredientCode: item.activeIngredientCode, activeIngredientName: item.activeIngredientName }))
+  // Filter fields
+  ActiveIngredient.afterRemote('find', async (ctx) => {
+    /**
+    * @todo Filter fields for "find" remote method
+    */
+    var items = ctx.result;
+    if (Array.isArray(items)) {
+      ctx.result = items.map(item => ({
+        activeIngredientCode: item.activeIngredientCode,
+        activeIngredientName: item.activeIngredientName
+      }))
     }
   })
 }
