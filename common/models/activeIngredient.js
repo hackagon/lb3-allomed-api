0b570fd961df6425eb7d2ca4f1db982ca6a378ce
hackagon <phonghiavan@gmail.com>
2019-10-08 08:53:43 +0700

    config product

--- common/models/activeIngredient.js
+++ common/models/activeIngredient.js
@@ -4,15 +4,25 @@ module.exports = (ActiveIngredient) => {
 
   // Filter fields
   ActiveIngredient.afterRemote('find', async (ctx) => {
-    /**
-    * @todo Filter fields for "find" remote method
-    */
     var items = ctx.result;
     if (Array.isArray(items)) {
       ctx.result = items.map(item => ({
+        id: item.id,
         activeIngredientCode: item.activeIngredientCode,
         activeIngredientName: item.activeIngredientName
       }))
     }
   })
-}
+
+  // ActiveIngredient.afterRemote('findById', (ctx) => {
+  //   const item = ctx.result;
+  //   item.categories.find()
+  //     .then(categories => {
+  //       console.log(categories)
+  //     })
+  // })
+
+  // ActiveIngredient.afterRemote('count', (ctx) => {
+  //   console.log(ctx.result);
+  // })
+} 
---
+++ common/models/product.js
@@ -0,0 +1,16 @@
+const _ = require('lodash');
+
+module.exports = (Product) => {
+
+  // Filter fields
+  Product.afterRemote('find', async (ctx) => {
+    var items = ctx.result;
+    if (Array.isArray(items)) {
+      ctx.result = items.map(item => ({
+        id: item.id,
+        productName: item.productName,
+        retailPrice: item.retailPrice
+      }))
+    }
+  })
+} 
