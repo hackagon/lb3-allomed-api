dbd19c6d3e926eeae00107299c6c3069f3911245
hackagon <phonghiavan@gmail.com>
2019-10-16 00:48:22 +0700

    options for cate and thera

--- common/models/form.js
+++ common/models/form.js
@@ -1,15 +1,45 @@
 const _ = require('lodash');
+const app = require('../../server/server')
 
+/**
+ * @todo: add inputs for form
+ * @todo: add options for input
+ */
 module.exports = (Form) => {
   Form.afterRemote('findOne', async (ctx) => {
+    const CategoryModel = app.models.Category;
+    const TherapyModel = app.models.Therapy;
+
     const form = ctx.result;
 
-    // const inputs = await form.inputs.find({ order: "displayIndex ASC" });
     let inputs = await form.inputs.find({});
 
     for (const i in inputs) {
       const inp = inputs[i];
-      const options = await inp.options.find();
+      let options;
+      if (inp.name === "categories") {
+        categories = await CategoryModel.find();
+        options = categories.map(cate => {
+          return {
+            id: cate.id,
+            displayEnglishName: cate.categoryName,
+            displayVietnameseName: cate.categoryName
+          }
+        })
+      } else if (inp.name === "therapies") {
+        therapies = await TherapyModel.find();
+        options = therapies.map(the => {
+          return {
+            id: the.id,
+            displayEnglishName: the.therapyName,
+            displayVietnameseName: the.therapyName
+          }
+        })
+      } else {
+        options = await inp.options.find();
+      }
+
+
       inputs[i] = { ...inputs[i].__data, options }
     }
 
