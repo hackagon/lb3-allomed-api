1b7863bfd45efdb8fed4e6036a62999253e2c4c7
Hackagon <phonghiavan@gmail.com>
2019-12-31 20:09:31 +0700

    remove generate-form api

--- server/boot/dataGenerationRoutes.js
+++ server/boot/dataGenerationRoutes.js
@@ -1,16 +1,16 @@
-const { generateFormWithInputs } = require('../controllers/generateForm');
+// const { generateFormWithInputs } = require('../controllers/generateForm');
 
-const _ = require('lodash');
-const forms = require('../../common/data/forms');
+// const _ = require('lodash');
+// const forms = require('../../common/data/forms');
 
-module.exports = (server) => {
-  const router = server.loopback.Router();
-  const restApiRoot = server.get('restApiRoot');
+// module.exports = (server) => {
+//   const router = server.loopback.Router();
+//   const restApiRoot = server.get('restApiRoot');
 
-  _.keys(forms).forEach(elm => {
-    const form = forms[elm];
-    router.post(`${restApiRoot}/generate-form/${elm}`, generateFormWithInputs(form))
-  })
+//   _.keys(forms).forEach(elm => {
+//     const form = forms[elm];
+//     router.post(`${restApiRoot}/generate-form/${elm}`, generateFormWithInputs(form))
+//   })
 
-  server.use(router);
-}
+//   server.use(router);
+// }
