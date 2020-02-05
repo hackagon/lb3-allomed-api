67c819d99a0bc4f19488951222151711bbee66ea
Hackagon <phonghiavan@gmail.com>
2020-02-05 22:17:45 +0700

    relation between product and price

--- common/models/InventoryLine.js
+++ common/models/InventoryLine.js
@@ -1,8 +1,8 @@
 const _ = require('lodash');
 const app = require('../../server/server')
 
-module.exports = (InventoryLine) => {
-  InventoryLine.observe("before save", async ctx => {
+module.exports = (ModelInventoryLine) => {
+  ModelInventoryLine.observe("before save", async ctx => {
     const instance__inventory_line = ctx.instance;
     if (instance__inventory_line.__data.conversionId) {
       const instance__conversion = await instance__inventory_line.conversion.get()
