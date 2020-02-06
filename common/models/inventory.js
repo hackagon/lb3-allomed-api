8f9d304c9830fe03bf005f0ede1b1ea701253830
Hackagon <phonghiavan@gmail.com>
2020-02-06 10:04:24 +0700

    delete

--- common/models/inventory.js
+++ common/models/inventory.js
@@ -4,6 +4,9 @@ const Promise = require("bluebird");
 
 module.exports = (ModelInventory) => {
 
+  /**
+   * @todo    add inventoryLine 
+   */
   ModelInventory.observe("after save", async ctx => {
     const ModelInventoryLine = app.models.InventoryLine
 
@@ -15,9 +18,22 @@ module.exports = (ModelInventory) => {
     })
   })
 
+  /**
+   * @todo    get inventory including inventoryLines
+   */
   ModelInventory.afterRemote("create", async ctx => {
     const instance__inventory = ctx.result;
     const inventoryLines = await instance__inventory.inventoryLines.find();
     _.set(instance__inventory, "__data.inventoryLines", inventoryLines);
+  })
+
+  /**
+   * @todo    delete inventory including inventoryLines
+   */
+
+  ModelInventory.observe("after delete", async ctx => {
+    const ModelInventoryLine = app.models.InventoryLine
+    const inventoryId = _.get(ctx, "where.id", "");
+    await ModelInventoryLine.destroyAll({ where: { inventoryId } })
   })
 }
