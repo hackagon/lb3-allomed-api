6e69b1d7e89690596d2737d40ce793636e2abd70
Hackagon <phonghiavan@gmail.com>
2020-02-18 08:57:00 +0700

    change name form

--- common/models/inventoryLine.js
+++ common/models/inventoryLine.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const app = require('../../server/server')
+const moment = require('moment');
 
 module.exports = (ModelInventoryLine) => {
   /**
@@ -12,5 +13,23 @@ module.exports = (ModelInventoryLine) => {
       instance__inventory_line.__data.converseQuantity = instance__inventory_line.__data.invoiceQuantity * instance__conversion.__data.quantity
       instance__inventory_line.__data.converseUnitPrice = instance__inventory_line.__data.invoiceUnitPrice / instance__conversion.__data.quantity
     }
+  })
+
+  /**
+   * @todo    create inventory Storing
+   */
+  ModelInventoryLine.observe("after save", async ctx => {
+    const ModelInventoryStoring = app.models.InventoryStoring;
+
+    const instance__inventory_line = ctx.instance;
+    const instance__inventory = await instance__inventory_line.inventory.get();
+    await ModelInventoryStoring.create({
+      inventoryLineId: instance__inventory_line.__data.id,
+      existingQuantity: 0,
+      importQuantity: instance__inventory_line.__data.quantity,
+      exportQuantity: 0,
+      month: moment(instance__inventory.__data.inventoryDate).format("MM"),
+      year: moment(instance__inventory.__data.inventoryDate).format("YYYY")
+    })
   })
 }
